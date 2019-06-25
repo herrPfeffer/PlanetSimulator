105679cc9b14028d424adb0004277c8496259248
herrPfeffer <tistian1316@yahoo.de>
2019-06-25 12:25:38 +0200

    example extension

--- Planetsimulator/Planetsimulator.py
+++ Planetsimulator/Planetsimulator.py
@@ -5,8 +5,10 @@ from AnimatedScatter import AnimatedScatter
 creator = PlanetCreator()
 
 #initialize variable number of planets
-creator.CreatePlanet(description='planet1', xPosition=0, yPosition=0, mass=150, speed=1)
-creator.CreatePlanet(description='planet2', xPosition=1, yPosition=1, mass=300, speed=1)
+creator.CreatePlanet(description='planet1', xPosition=0, yPosition=1, mass=150, speed=1)
+creator.CreatePlanet(description='planet2', xPosition=1, yPosition=2, mass=300, speed=1)
+creator.CreatePlanet(description='planet2', xPosition=2, yPosition=3, mass=300, speed=1)
+creator.CreatePlanet(description='planet2', xPosition=3, yPosition=4, mass=300, speed=1)
 
 #Show Planets in plotter
 scatter = AnimatedScatter("Planetsimulator", "x-Axis", "y-Axis", 2, creator)
