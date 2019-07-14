99256c65cc309495c66a063eb506c8461921a05c
Besitzer <Besitzer@R5567952>
2019-07-14 10:32:49 +0200

    comments changed

--- Planetsimulator/Planetsimulator.py
+++ Planetsimulator/Planetsimulator.py
@@ -6,7 +6,6 @@ manager = PlanetManager(5)
 
 #initialising a variable number of planets
 #distance in m, masses in kg, speed in m/s
-#manager.create_planet(description='mercury', x_position=7.376*10**12, y_position=7.376*10**12-5.791*10**10, mass=3.285*10**10, x_speed=33490, y_speed=33490)
 manager.create_planet(description='mercury', x_position=7.376*10**12, y_position=7.376*10**12-5.791*10**10, mass=3.285*10**10, x_speed=33490, y_speed=33490)
 manager.create_planet(description='venus', x_position=7.376*10**12, y_position=7.376*10**12-1.082*10**11, mass=4.867*10**24, x_speed=24763, y_speed=24763)
 manager.create_planet(description='earth', x_position=7.376*10**12, y_position=7.376*10**12-1.500*10**11, mass=5.972*10**24, x_speed=21058, y_speed=21058)
